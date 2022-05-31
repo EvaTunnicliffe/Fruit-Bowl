3ef9dad4aec1ee029ecd230386e362778b809e6f
EvaTunnicliffe <105611389+EvaTunnicliffe@users.noreply.github.com>
2022-05-31 14:51:27 +1200

    Add files via upload

--- Fruitbowl_1.py
+++ Fruitbowl_1.py
@@ -1,3 +1,12 @@
+def get_integer(m):
+    my_integer = int(input(m))
+    return my_integer
+
+
+def get_string(m):
+    my_string = input(m)
+    return my_string
+
 
 def print_fruit(l):
     for x in l:
@@ -5,21 +14,40 @@ def print_fruit(l):
         print(output)
 
 
+def print_with_indexes(l):
+    for i in range(0, len(l)):
+        output = "{} : {} : {}".format(i, l[i][0], l[i][1])
+        print(output)
+
+
+def add_fruit(l):
+    print_with_indexes(l)
+    choice = get_integer("Choose your fruit that you would like to add to: ")
+    quantity = get_integer("How many would you like to add: ")
+    l[choice][1] += quantity
+    output = "{} {} have been added to the list".format(quantity, l[choice][0])
+    print(output)
+
+
 def main():
+    fruit_list = [["Apples", 10],
+                  ["Grapes", 500],
+                  ["Lemons", 2]]
     run = True
-    while run == True:
-        fruit_list = (["Apples", 10],
-                      ["Grapes", 500],
-                      ["Lemons", 2])
+    while run is True:
+
         # Things to print out
         # menu
         menu = '''
         P: Print out list of fruit
+        A: Adding to fruit
         Q: Quit'''
         print(menu)
-        choice = input("What would you like to do?")
+        choice = input("What would you like to do? ")
         if choice == "P":
             print_fruit(fruit_list)
+        elif choice == "A":
+            add_fruit(fruit_list)
         elif choice == "Q":
             run = False
 
